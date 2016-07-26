1846434758e9f7a14e1476ffa24750a77a102e0d
ctpatey <ctpatey@gmail.com>
2016-07-26 10:59:09 -0400

    update for possible job

--- js/app.js
+++ js/app.js
@@ -1,72 +1,69 @@
 // Data. Including Ajax request for relevant info from Wikipedia
 var historicLocations = 
-	[{
-		"name": "Holocaust Memorial",
-		"lat": 42.361211,
-		"lng": -71.057086
-	}, {
-		"name": "Old State House",
-		"lat": 42.358781,
-		"lng": -71.057448
-	}, {
-		"name": "Paul Revere's House",
-		"lat": 42.363668,
-		"lng": -71.053757
-	}, {
-		"name": "Old North Church",
-		"lat": 42.366388,
-		"lng": -71.054360
-	}, {
-		"name": "Old South Meeting House",
-		"lat": 42.356970,
-		"lng": -71.058331
-	}, {
-		"name": "Boston Tea Party Ships and Museum",
-		"lat": 42.352175,
-		"lng": -71.051213
-	}]
+  [{
+    "name": "Holocaust Memorial",
+    "lat": 42.361211,
+    "lng": -71.057086,
+    "wikiPageid": 15507987
+  }, {
+    "name": "Old State House",
+    "lat": 42.358781,
+    "lng": -71.057448,
+    "wikiPageid": 1418090
+  }, {
+    "name": "Paul Revere's House",
+    "lat": 42.363668,
+    "lng": -71.053757,
+    "wikiPageid": 2709194
+  }, {
+    "name": "Old North Church",
+    "lat": 42.366388,
+    "lng": -71.054360,
+    "wikiPageid": 667505
+  }, {
+    "name": "Old South Meeting House",
+    "lat": 42.356970,
+    "lng": -71.058331,
+    "wikiPageid": 1115450
+  }, {
+    "name": "Boston Tea Party Ships and Museum",
+    "lat": 42.352175,
+    "lng": -71.051213,
+    "wikiPageid": 4608353
+  }]
 
 
 
 function loadData (){
 
-	var $wikiElem = $('#wikipedia-links');
+  var $wikiElem = $('#wikipedia-links');
+  
+  var wikiUrl = "https://en.wikipedia.org/w/api.php?"
 
-	var wikiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=New_England_Holocaust_Memorial&format=json&callback=wikiCallback"
+  $.ajax({
+    url: wikiUrl,
+    dataType: "jsonp",
+    success: function( response ) {
 
-	$.ajax({
-		url: wikiUrl,
-		dataType: "jsonp",
-		success: function( response ) {
-
-			helloWorld(response);
-			var articleList = response[1];
-
-
-			console.log(response)
-
-			for (var i = 0; i < articleList.length; i++) {
-				articleStr = articleList[i];
-
-				var url = "http://en.wikipedia.org/wiki/" + articleStr;
-				$wikiElem.append("<li><a target='_blank' href='" + url + "'>" + articleStr + "</a></li>")
-			};
+      
+      var articleList = response[1];
 
 
-		}
-	});
+      console.log(response)
+
+      for (var i = 0; i < articleList.length; i++) {
+        articleStr = articleList[i];
+
+        var url = "http://en.wikipedia.org/wiki/" + articleStr;
+        $wikiElem.append("<li><a target='_blank' href='" + url + "'>" + articleStr + "</a></li>")
+      };
+
+
+    }
+  });
 };
 
-console.log(loadData());
-
-function helloWorld(response){
-	console.log(response)
-}
-
-
-
-
-
+loadData();
 
 
 
@@ -85,76 +82,57 @@ function initMap() {
   });
 
   
+  ko.applyBindings(new ViewModel());
 
-
-  
-
-
-  	var largeInfoWindow = new google.maps.InfoWindow();
-  	
-  	var markers = []
-
-  	for (let value of historicLocations) {
-  		var marker = new google.maps.Marker({
-		  	position: { lat: value.lat, lng: value.lng },
-		  	map: map,
-		  	animation: google.maps.Animation.DROP,
-		  	title: value.name
-		  });
-
-  		markers.push(marker);
-
-  	};
-
-  	for (let value of markers){
-	  	value.addListener('click', function (){
-  			populateInfoWindow(this, largeInfoWindow);
-  			
-  			this.setAnimation(google.maps.Animation.BOUNCE);
-			  	
-  		});
-	  };
-
-  	function populateInfoWindow (marker, infowindow) {
-  		if (infowindow.marker != marker) {
-  			infowindow.marker = marker;
-  			infowindow.setContent('<div>' + marker.title + '</div>');
-  			infowindow.open(map, marker);
-  	
-  		}
-  	}
-  	
-  	
 }
 
 
-
 // Here's my data model
-var ViewModel = function(first, last) {
-    this.firstName = ko.observable(first);
-    this.lastName = ko.observable(last);
- 
-    this.fullName = ko.pureComputed(function() {
-        // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
-        return this.firstName() + " " + this.lastName();
-    }, this);
+var ViewModel = function() {
+    var self = this;
+    self.locations = ko.observableArray(historicLocations);
+    
+    
+    self.clickFunction = function() {
+      
+    }
+
+    var largeInfoWindow = new google.maps.InfoWindow();
+
+    self.locations().forEach(function(location) {
+
+	    var marker = new google.maps.Marker ({
+	      map: map,
+	      position: { lat: location.lat, lng: location.lng },
+	      title: location.name,
+	      animation: google.maps.Animation.DROP    
+
+	    })
+	      location.marker = marker;
+
+	      marker.addListener('click', function(){
+	           
+		      self.populateInfoWindow(marker, largeInfoWindow);
+		      marker.setAnimation(google.maps.Animation.BOUNCE);
+		      setTimeout(function(){ 
+	          location.marker.setAnimation(null);
+	          }, 750);
+	    });
+       
+    });
+
+
+    self.populateInfoWindow = function(marker, infowindow) {
+      if (infowindow.marker != marker) {
+        infowindow.marker = marker; 
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+    
+      }
+    }
+    
+    
 };
- 
-
-
-$(document).ready(function(){
-    ko.applyBindings(new ViewModel("Planet", "Earth"));
-});
-
-
-
-
-
-
-
-
-
-
 
 
 
